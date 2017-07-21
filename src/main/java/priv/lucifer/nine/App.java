437b467846759e6a58577a495036e643dc209521
LuciferWong <462881925@qq.com>
2017-07-21 21:14:06 +0800

    2017-7-21

---
+++ src/main/java/priv/lucifer/nine/App.java
@@ -0,0 +1,12 @@
+package priv.lucifer.nine;
+import javax.swing.*;
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		javax.swing.JTree tree = new JTree();
+		System.out.println("Hello World!");
+	}
+}
